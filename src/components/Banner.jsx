a8fc2988da6a331251379de4593d9660fd6208c3
risalat345 <ashrafuddinbbhuiyanrisalat>
2025-02-23 20:42:27 +0600

    Initial commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import banner from "../assets/frontend_assets/banner.png";
+
 const wordVariants = {
   hidden: { x: "100%", opacity: 0 },
   visible: (custom) => ({
@@ -8,45 +8,64 @@ const wordVariants = {
     opacity: 1,
     transition: { duration: 0.8, ease: "easeInOut", delay: custom * 0.3 },
   }),
-}
+};
+
 const Banner = () => {
   const text1 = ["Your", "Favorite", "Food"];
   const text2 = ["Just", "A", "Tap", "Away!"];
+
   return (
-    <div id="#home" className="relative w-full mt-20">
-      <img src={banner} alt="Banner" className="w-full h-fit rounded-lg" />
-      <div className="absolute top-2 left-36 md:top-16 md:left-90 text-center">
-        <div className="flex space-x-1.5 md:space-x-3">
-          {text1.map((word, i) => (
-            <motion.h1
-              key={i}
-              custom={i}
-              initial="hidden"
-              animate="visible"
-              variants={wordVariants}
-              className="md:text-5xl font-bold text-black"
-            >
-              {word}
-            </motion.h1>
-          ))}
+    <div id="#home" className="bg-orange-600 w-full mt-20 h-fit md:flex">
+      {/* Left Side */}
+      <div className="left md:w-6/12 flex justify-center items-center">
+        <img
+          width={250}
+          className="md:w-[300px] w-[200px]"
+          src="https://static.vecteezy.com/system/resources/previews/023/743/925/non_2x/scooter-with-delivery-man-flat-cartoon-character-fast-courier-restaurant-food-service-mail-delivery-service-a-postal-employee-the-determination-of-geolocation-using-electronic-device-free-png.png"
+          alt=""
+        />
+      </div>
+
+      {/* Right Side */}
+      <div className="right md:w-6/12 flex justify-center items-center p-5 md:p-14">
+        <div className="text-center">
+          <div className="flex space-x-1 md:space-x-3 justify-center">
+            {text1.map((word, i) => (
+              <motion.h1
+                key={i}
+                custom={i}
+                initial="hidden"
+                animate="visible"
+                variants={wordVariants}
+                className="text-3xl md:text-4xl font-bold text-black"
+              >
+                {word}
+              </motion.h1>
+            ))}
+          </div>
+
+          <div className="flex space-x-1 md:space-x-3 mt-2 justify-center">
+            {text2.map((word, i) => (
+              <motion.h1
+                key={i}
+                custom={i}
+                initial="hidden"
+                animate="visible"
+                variants={wordVariants}
+                className="text-2xl md:text-4xl font-bold text-black"
+              >
+                {word}
+              </motion.h1>
+            ))}
+          </div>
+
+          <button className="px-5 md:px-10 text-base md:text-md py-1 md:py-2 hover:bg-black hover:text-white transition-all duration-500 font-bold border border-black rounded-lg my-3 md:my-5 cursor-pointer">
+            Order Now
+          </button>
         </div>
-        <div className="flex space-x-2 md:space-x-3 mt-2">
-          {text2.map((word, i) => (
-            <motion.h1
-              key={i}
-              custom={i}
-              initial="hidden"
-              animate="visible"
-              variants={wordVariants}
-              className="md:text-5xl text-2xl font-bold text-black"
-            >
-              {word}
-            </motion.h1>
-          ))}
-        </div>
-        <button className="md:px-10 px-5 text-sm md:text-1xl md:py-2 py-1 hover:bg-black hover:text-white transition-all duration-500 font-bold border border-black rounded-lg my-2 md:my-5 cursor-pointer">Order Now</button>
       </div>
     </div>
-  )
-}
+  );
+};
+
 export default Banner;
--- src/components/LoginPopUp.jsx
+++ src/components/LoginPopUp.jsx
@@ -5,10 +5,14 @@ const LoginPopUp = ({setshowSignIn}) => {
   return(
   <div className="login-popup md:mt-10">
       <form className='bg-white fadeIn popup-container p-3 rounded-2xl shadow-lg w-96 mx-auto text-center'>
-      <div onClick={()=>setshowSignIn(false)} className="absolute right-75 cursor-pointer ">
+    <div className="flex items-center justify-between">
+    <div></div>
+        <div>{user==="Sign Up"?<h1 className='text-2xl font-bold text-orange-600 mb-2'>Sign Up</h1>:<h1 className='text-2xl font-bold text-orange-600 mb-2'>Login</h1>}</div>
+      <div onClick={()=>setshowSignIn(false)} className="cursor-pointer ">
       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><g fill="none"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="#ea580c" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2m0 2a8 8 0 1 0 0 16a8 8 0 0 0 0-16M9.879 8.464L12 10.586l2.121-2.122a1 1 0 1 1 1.415 1.415l-2.122 2.12l2.122 2.122a1 1 0 0 1-1.415 1.415L12 13.414l-2.121 2.122a1 1 0 0 1-1.415-1.415L10.586 12L8.465 9.879a1 1 0 0 1 1.414-1.415"/></g></svg>
 </div>
-      <h1 className='text-2xl font-bold text-orange-600 mb-2'>Login</h1>
+    </div>
+      
       <div className='flex flex-col gap-2'>
       {user==="Sign Up"?<input className='w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500' placeholder='Enter Your Name'
           type='text' required />:""}
